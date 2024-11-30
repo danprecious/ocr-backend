ff4de2ef78b617d67193c665a0091c51fb471433
Kayode Dan Precious <danielkayode222@gmail.com>
2024-11-30 12:08:48 +0100

    rate-limiting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { config } from "@dotenvx/dotenvx";
+import { rateLimit } from "express-rate-limit";
 
 import router from "./routes/apiRoutes.js";
 
@@ -8,7 +9,16 @@ config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
+app.use(limiter);
 
 app.use(cors());
 
@@ -25,6 +35,11 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(PORT, () => {
-  console.log(`app running on  http://localhost:${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(`Error starting the server: ${err.message}`);
+    process.exit(1); // Exit the process with a failure code
+  } else {
+    console.log(`Server is running on port ${PORT}`);
+  }
 });
